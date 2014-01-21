745d4465a9511481021477a35929e6ab72d329e7
Jacob Morrison <jomorrison@gmail.com>
2014-01-20 20:36:53 -0800

    first commit

---
+++ templates/docset/Renderer.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\apidoc\templates\docset;
+use Yii;
+use SQLite3;
+
+/**
+ *
+ * @author Carsten Brandt <mail@cebe.cc>
+ * @since 2.0
+ */
+class Renderer extends \yii\apidoc\templates\offline\Renderer
+{
+	public $db;
+
+	public function render($context, $controller)
+	{
+		if (substr($this->targetDir, -7) !== '.docset') {
+			$this->targetDir .= '.docset';
+		}
+		$originalTargetDir = $this->targetDir;
+		$sqliteDb = $originalTargetDir . DIRECTORY_SEPARATOR . 'Contents' . DIRECTORY_SEPARATOR .'Resources' . DIRECTORY_SEPARATOR. 'docSet.dsidx';
+		$this->targetDir = $originalTargetDir . DIRECTORY_SEPARATOR . 'Contents' . DIRECTORY_SEPARATOR .'Resources' . DIRECTORY_SEPARATOR . 'Documents';
+
+		if (!is_dir($this->targetDir)) {
+			mkdir($this->targetDir, 0755, true);
+		}
+		if (is_file($sqliteDb)) {
+			unlink($sqliteDb);
+		}
+		if(!($this->db = new SQLite3($sqliteDb))) {
+			return false;
+		}
+
+    	$this->db->query('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);');
+    	$this->db->query('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);');
+    	foreach ($context->classes as $class)
+    	{
+    		$this->addType($class->name, 'Class', $this->generateFileName($class->name));
+    		$this->typeHandle($class);
+    	}
+
+    	foreach ($context->traits as $trait)
+    	{
+    		$this->addType($trait->name, 'Trait', $this->generateFileName($trait->name));
+    		$this->typeHandle($trait);
+    	}
+
+    	foreach ($context->interfaces as $interface)
+    	{
+    		$this->addType($interface->name, 'Interface', $this->generateFileName($interface->name));
+    		$this->typeHandle($interface);
+    	}
+
+		return parent::render($context, $controller);
+	}
+	protected function typeHandle($type)
+	{
+		if (!empty($type->methods)) {
+			foreach ($type->methods as $method) {
+				if ($type->name != $property->definedBy) { continue; }
+				$this->addType($type->name .'::'. $method->name, 'Method', $this->generateFileName($type->name) .'#'. $method->name . '()-detail');
+			}
+		}
+		if (!empty($type->properties)) {
+			foreach ($type->properties as $property) {
+				if ($type->name != $property->definedBy) { continue; }
+				$this->addType($type->name .'::'. $property->name, 'Property', $this->generateFileName($type->name) .'#'. $property->name . '-detail');
+			}
+		}
+
+	}
+	protected function addType($name, $type, $path)
+	{
+		return $this->db->query('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (\''.SQLite3::escapeString($name).'\', \''.SQLite3::escapeString($type).'\', \''.SQLite3::escapeString($path).'\');');
+	}
+}
